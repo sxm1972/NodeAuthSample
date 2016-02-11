333eea276f670db266726067eb21f84f825ed795
Sundeep Mallya <sxm1972@yahoo.com>
2016-02-11 10:54:37 +0000

    Used wiredep and inject to update client side css and js dependencies

--- server.js
+++ server.js
@@ -2,12 +2,14 @@
 		var app = express();
 		var port = process.env.PORT || 3000;
 
+		app.use(express.static('public'));
+
+		app.use(express.static('src/views'));
 		app.get('/', function(req, res) {
-		    res.send('Welcome to my new API');
+			res.render('index');
 		});
 
 		app.listen(port, process.env.IP, 1, function() {
-			
+
 			console.log('Listening on ' + process.env.IP + ':' + port);
 		});
-		
