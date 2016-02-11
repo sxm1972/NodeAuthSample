2c7aea7ddfb7711a0696f26ff846007da93af11c
Sundeep Mallya <sxm1972@yahoo.com>
2016-02-11 10:58:51 +0000

    Updated client side dependencies using wiredep and inject. So removed bootstrap and jQuery files from my app folders

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@
 	var nodemon = require('gulp-nodemon');
 	var jshint = require('gulp-jshint');
 	var jscs = require('gulp-jscs');
-
+	var wiredep = require('wiredep').stream;
+	var inject = require('gulp-inject');
+	var debug = require('gulp-debug');
+	
 	var jsFiles = ['*.js', 'src/**/*.js'];
 	gulp.task('code', function() {
 		return gulp.src(jsFiles).pipe(jscs());
@@ -17,7 +20,27 @@
 			.pipe(jscs());
 	});
 
-	gulp.task('default', function() {
+	var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {read: false});
+	var injectOptions = {
+		ignorePath: '/public'	
+	};
+	var options = {
+		bowerJson: require('./bower.json'),
+		directory: './public/lib',
+		ignorePath: '../../public'
+	};
+	
+	gulp.task('inject', function() {
+		return gulp.src('./src/views/*.html')
+				//.pipe(debug())
+				.pipe(wiredep(options))
+				//.pipe(debug())
+				.pipe(inject(injectSrc, injectOptions))
+				//.pipe(debug())
+				.pipe(gulp.dest('./src/views'));
+				
+	});
+	gulp.task('default', ['style', 'inject'], function() {
 		nodemon({
 				script: 'server.js',
 				ext: 'js',
